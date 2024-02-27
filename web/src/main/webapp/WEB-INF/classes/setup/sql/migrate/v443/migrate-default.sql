70f1a847806b7076d920dfba3158e384fec34353
Francois Prunayre <fx.prunayre@gmail.com>
2024-02-27 07:57:15 +0100

    Standard / ISO19115-3 / Schema / Move from srv 2.1 to 2.0
    SRV XSD 2.1 was removed on TC211 https://github.com/ISO-TC211/XML/commit/06bb6bcc722bf1f70dc3ec822d45ca1decd2ad3d
    after we added the schema in GeoNetwork. 2.0 is the version to use.
    
    Following discussion https://github.com/geonetwork/core-geonetwork/issues/7578#issuecomment-1964753641

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v443/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v443/migrate-default.sql
@@ -1,7 +1,9 @@
-UPDATE Settings SET value='4.4.3' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Metadata SET data = replace(data, 'http://standards.iso.org/iso/19115/-3/srv/2.1', 'http://standards.iso.org/iso/19115/-3/srv/2.0') WHERE data LIKE '%http://standards.iso.org/iso/19115/-3/srv/2.1%' AND schemaId = 'iso19115-3.2018';
 
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/documentation/url', 'https://docs.geonetwork-opensource.org/{{version}}/{{lang}}', 0, 570, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/userFeedback/metadata/enable', 'false', 2, 1913, 'n');
 
+
+UPDATE Settings SET value='4.4.3' WHERE name='system/platform/version';
+UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
